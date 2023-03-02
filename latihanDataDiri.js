6c2c6423f610a192790e59013c80ed55aa3ff35a
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-02 22:02:26 +0800

    first commit

---
+++ latihanDataDiri.js
@@ -0,0 +1,5 @@
+function dataDiri(nama,asal="  Jimbaran ",suku = " Bali ", asal_kampus = " Universitas Udayana ", jurusan = " Teknik Elektro "){
+  console.log("Perkenalkan nama saya " + nama + " saya berasal dari "+ asal +" suku saya "+ suku + "  saya berkuliah di "+ asal_kampus + "jurusan saya " + jurusan)
+}
+dataDiri ("polvi")
+dataDiri ("pradnya","Kuta")
