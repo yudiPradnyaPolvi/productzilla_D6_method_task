b5228593237f8d394b297781656e1466a30ea3ab
yudiPradnyaPolvi <pradnyakemenuh@gmail.com>
2023-03-02 22:20:37 +0800

    first commit

---
+++ Revisi_latihanAritmatika.js
@@ -0,0 +1,42 @@
+console.log ("function method")
+function hasilPerhitungan1 (x){
+    const hasil1 = x**2
+    return hasil1 
+}
+let x=4
+
+const hasil1 = hasilPerhitungan1 (x)
+console.log (hasil1)
+
+function hasilPerhitungan2 (a,b){
+    const hasil2 = (a*2)+(b*2)
+    return hasil2 
+}
+let a=2,b=3
+const hasil2 = hasilPerhitungan2 (a,b)
+console.log (hasil2)
+
+
+function hasilPerhitungan3 (h,i){
+    const hasil3 = (h*i)/2
+    return hasil3 
+}
+let h=1,i=3
+const hasil3 = hasilPerhitungan3 (h,i)
+console.log (hasil3)
+
+function hasilPerhitungan4 (e,f){
+    const hasil4 = e*f
+    return hasil4 
+}
+let e=2,f=3
+const hasil4 = hasilPerhitungan4 (e,f)
+console.log (hasil4)
+
+function hasilPerhitungan5 (phi,r){
+    const hasil5 = phi*2*r
+    return hasil5 
+}
+let phi=3.14,r=10
+const hasil5 = hasilPerhitungan5 (phi,r)
+console.log (hasil5)
